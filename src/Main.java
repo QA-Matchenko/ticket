a01b4c3496747a10784b63de381c6c45231e92d5
Darya M <matchenko.darya@mail.ru>
2024-12-10 21:14:20 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 43670;
+        int mile = (price / 20);
+        System.out.println("Начислено миль:" + mile);
+
+    }
+}
